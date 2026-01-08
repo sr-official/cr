81c6fcdb7ad2dcec7b434ad28e250195f51bde8d
srjibon <srjibonofficial@gmail.com>
2026-01-09 00:21:27 +0600

    update font, form

--- assets/links/links.js
+++ assets/links/links.js
@@ -33,16 +33,22 @@ const gridContainer = document.getElementById('gridContainer');
 
 // Example data for clickable divs
 const data = [
-		{ text: 'PCIU Cultural Forum', url: 'https://www.facebook.com/pciupccf1/' },
-		{ text: 'PCIU Sports Forum', url: 'https://www.facebook.com/groups/178717602310691/user/100063706102516/' },
-		{ text: 'PCIU Volunteers', url: 'https://www.facebook.com/groups/178717602310691/user/100089565117756/' },
-		{ text: 'PCIU Debate Forum', url: 'https://www.facebook.com/groups/178717602310691/user/100063855258518/' },
-		{ text: 'PCIU Robotics Club', url: 'https://www.facebook.com/groups/178717602310691/user/100063785072774/' },
-		{ text: 'PCIU Civil Forum', url: 'https://www.facebook.com/groups/178717602310691/user/100083964973198/' },
-		{ text: 'PCIU Computer Club', url: 'https://www.facebook.com/groups/178717602310691/user/100092263279944/' },
-		{ text: 'PCIU EEE Club', url: 'https://www.facebook.com/groups/178717602310691/user/61557189404115/' },
-		{ text: 'IEEE PCIU Student Branch', url: 'https://www.facebook.com/groups/845533542452997'},
-		{ text: 'PCIU Textile Club', url: 'https://www.facebook.com/groups/178717602310691/user/61557189404115/' },
+	{ text: 'PCIU Debate Forum', url: 'https://www.facebook.com/groups/178717602310691/user/100063855258518/' },
+	{ text: 'PCIU Sports Forum', url: 'https://www.facebook.com/groups/178717602310691/user/100063706102516/' },
+	{ text: 'PCIU Cultural Forum', url: 'https://www.facebook.com/pciupccf1/' },
+	{ text: 'PCIU Volunteers', url: 'https://www.facebook.com/groups/178717602310691/user/100089565117756/' },
+	{ text: 'PCIU Rover Scout Group', url: 'https://www.facebook.com/groups/178717602310691/user/61574950585244/' },
+	{ text: 'PCIU Business Club', url: 'https://www.facebook.com/groups/178717602310691/user/100064322547208/' },
+	{ text: 'PCIU Textile Club', url: 'https://www.facebook.com/groups/178717602310691/user/61557189404115/' },
+	{ text: 'PCIU Robotics Club', url: 'https://www.facebook.com/groups/178717602310691/user/100063785072774/' },
+	{ text: 'IEEE PCIU Student Branch', url: 'https://www.facebook.com/groups/178717602310691/user/100057450609287/' },
+	{ text: 'PCIU Civil Engineering Forum', url: 'https://www.facebook.com/groups/178717602310691/user/100083964973198/' },
+	{ text: 'PCIU Computer Club', url: 'https://www.facebook.com/groups/178717602310691/user/100092263279944/' },
+	{ text: 'PCIU Media Club', url: 'https://www.facebook.com/groups/178717602310691/user/61567010822501/' },
+	{ text: 'PCIU English Club', url: 'https://www.facebook.com/groups/178717602310691/user/100064170573912/' },
+	{ text: "PCIU Fashion Designer's Club", url: 'https://www.facebook.com/groups/178717602310691/user/100066802296909/' },
+	{ text: 'PCIU EEE Club', url: 'https://www.facebook.com/groups/178717602310691/user/61557189404115/' },
+	{ text: 'PCIU Networking & Placement Cell', url: 'https://www.facebook.com/groups/pciuplacementcell' },
 ];
 
 // Function to add a clickable div
